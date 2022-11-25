925a258be2f0a559d70f23c740e79a4785e17a77
Alexander <renyreny125tt@gmail.com>
2022-11-25 19:10:40 +0300

    feat: add middleware check token

--- src/app.js
+++ src/app.js
@@ -3,12 +3,15 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import todoRoutes from './resources/todo/todo.router';
 import loginRoutes from './resources/login/login.router';
+import { checkToken } from './tokens/check.token';
 
 export const app = new Express();
 
 app.use(cors());
 
 app.use(bodyParser.json());
+
+app.use('', checkToken);
 
 app.use('', loginRoutes);
 app.use('', todoRoutes);
---
+++ src/tokens/check.token.js
@@ -0,0 +1,47 @@
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET_KEY } from '../common/config';
+import statusCode from '../common/status.code';
+
+/**
+ * Intermediate function
+ * Checks for a valid token
+ * @param req first argument request
+ * @param res second argument response
+ * @param next for next
+ * @returns void
+ */
+const checkToken = (req, res, next) => {
+  const textError = 'Authorization Error!';
+  const path = req.path;
+  const autHeader = req.headers.authorization;
+
+  if (path === '/login/registration'|| path === '/login/authorization' || path === '/') {
+    return next();
+  }
+
+  if (autHeader === undefined) {
+    console.error(textError);
+    res.status(statusCode.FORBIDDEN).send(textError);
+    return;
+  } else {
+    const [type, token] = autHeader.split(' ');
+
+    if (type === 'Bearer' && token) {
+      try {
+        jwt.verify(token, JWT_SECRET_KEY);
+      } catch (error) {
+        console.error(textError);
+        res.status(statusCode.FORBIDDEN).send(textError);
+        return;
+      }
+    } else {
+      console.error(textError);
+      res.status(statusCode.FORBIDDEN).send(textError);
+      return;
+    }
+  }
+
+  next();
+};
+
+export { checkToken };
