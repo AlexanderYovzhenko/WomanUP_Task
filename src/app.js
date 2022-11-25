e65e04c721abee090de16e0b57a63df3d06bf382
Alexander <renyreny125tt@gmail.com>
2022-11-25 22:53:24 +0300

    feat: add static download

--- src/app.js
+++ src/app.js
@@ -1,17 +1,24 @@
-import Express from 'express';
+import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import todoRoutes from './resources/todo/todo.router';
 import loginRoutes from './resources/login/login.router';
 import { checkToken } from './tokens/check.token';
 
-export const app = new Express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const app = express();
 
 app.use(cors());
 
 app.use(bodyParser.json());
 
-app.use('', checkToken);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(checkToken);
 
 app.use('', loginRoutes);
 app.use('', todoRoutes);
