139ad17d573e6045f46dade091695f3644b7c898
Alexander <renyreny125tt@gmail.com>
2022-12-11 23:11:00 +0300

    refactor: change settings vercel

--- src/app.js
+++ src/app.js
@@ -9,14 +9,10 @@ import todoRoutes from './resources/todo/todo.router';
 import loginRoutes from './resources/login/login.router';
 import fileRoutes from './resources/file/file.router';
 
-import { PORT, URL_DB } from './common/config';
-import mongoose from 'mongoose';
-
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const app = express();
+export const app = express();
 
 app.use(cors());
 
@@ -31,13 +27,3 @@ app.use(checkToken);
 app.use('', loginRoutes);
 app.use('', todoRoutes);
 app.use('', fileRoutes);
-
-try {
-  mongoose.connect(URL_DB);
-  app.listen(PORT, () =>
-    console.info(`App is running on ${PORT}`)
-  );
-} catch (err) {
-  console.error(err);
-  process.exit(1);
-}
